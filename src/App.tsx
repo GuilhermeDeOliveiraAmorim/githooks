90ec250ed8dadb7757464ca8acede7de85ebd0af
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-01 13:01:19 -0300

    feat: learning about hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-    return <div className="App"></div>;
+    const [repositories, setRepositories] = useState([
+        { id: 1, name: "repo-teste-1" },
+        { id: 2, name: "repo-teste-2" },
+        { id: 3, name: "repo-teste-3" },
+    ]);
+
+    function handleAddRepository() {
+        const idN = Math.random();
+        setRepositories([
+            ...repositories,
+            { id: idN, name: "novo-repo-"+idN }
+        ]);
+    }
+
+    return (
+        <>
+            <ul>
+                {repositories.map((repo) => (
+                    <li key={repo.id}>{repo.name}</li>
+                ))}
+            </ul>
+            <button onClick={handleAddRepository}>Add Repository</button>
+        </>
+    );
 }
 
 export default App;
