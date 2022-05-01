1269f8969511a4a134ff13c4ff59299cf07e414e
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-01 13:58:50 -0300

    feat: changing the document.title with useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -14,9 +14,14 @@ export default function App() {
         fetchData();
     }, []);
     
+    useEffect(() => {
+        const filtered = repositories.filter(repo => repo.favorite);
+        document.title = `You have ${filtered.length} favorite repositories`;
+    }, [repositories]);
+    
     function handleFavorite(id: number) {
         const newRepositories = repositories.map(repo => {
-            return repo.id === id ? {...repo, favorite: true} : repo
+            return repo.id === id ? {...repo, favorite: !repo.favorite} : repo
         });
         setRepositories(newRepositories);
     }
@@ -25,10 +30,8 @@ export default function App() {
         <ul>
             {repositories.map((repo) => (
                 <li key={repo.id}>
-                    {repo.name}{repo.favorite && <span> (Favorite)</span>}
-                    <button onClick={() => handleFavorite(repo.id)}>
-                        Add
-                    </button>
+                    {repo.name}{repo.favorite && <span> (Favorite) </span>}
+                    <button onClick={() => handleFavorite(repo.id)}>+</button>
                 </li>
             ))}
         </ul>
