afb4df80af9af113386f19882031f863b9eecb2d
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2022-05-01 13:34:43 -0300

    feat: adding a button to bookmark the repository

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,27 @@ export default function App() {
             const response = await axios(
                 "https://api.github.com/users/GuilhermeDeOliveiraAmorim/repos"
             );
-
             setRepositories(response.data);
         };
-
         fetchData();
     }, []);
+    
+    function handleFavorite(id: number) {
+        const newRepositories = repositories.map(repo => {
+            return repo.id === id ? {...repo, favorite: true} : repo
+        });
+        setRepositories(newRepositories);
+    }
 
     return (
         <ul>
             {repositories.map((repo) => (
-                <li key={repo.id}>{repo.name}</li>
+                <li key={repo.id}>
+                    {repo.name}{repo.favorite && <span> (Favorite)</span>}
+                    <button onClick={() => handleFavorite(repo.id)}>
+                        Add
+                    </button>
+                </li>
             ))}
         </ul>
     );
